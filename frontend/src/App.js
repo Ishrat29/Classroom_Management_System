d346789f3172f9cba0737918e32de3908cad41a4
Ishrat Jahan <52996520+Ishrat29@users.noreply.github.com>
2022-05-01 23:10:47 +0600

    Update App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-
 import {BrowserRouter as Router,Switch,Route} from 'react-router-dom';
 import Login from './components/Login/Login';
 
@@ -13,24 +12,3 @@ function App() {
 
 export default App;
 
-// import React from 'react';
-// import './App.css';
-// import "bootstrap/dist/css/bootstrap.css";
-// import {Button, Alert, Row, Col} from 'react-bootstrap';
-// import LeftSide from "./components/LeftSide";
-// import Menu from "./components/Menu";
-// import RightSide from "./components/RightSide";
-// function App() {
-//   return (
-//     <div className="App">
-//       <Menu />
-//       <Row className="landing">
-//         <Col ><LeftSide /></Col>
-        
-//         <Col ><RightSide /></Col>
-//       </Row>
-//     </div>
-//   );
-// }
-
-// export default App;
