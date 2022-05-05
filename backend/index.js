7465ac4ce045e0e03042f28790fab56f3c20142c
Shrestha Datta <52985769+Shrestha30@users.noreply.github.com>
2022-05-05 21:34:37 +0600

    backend, frontend and UI of AddEditStudent page
    install dependencies: axios, cors

---
+++ backend/index.js
@@ -0,0 +1,114 @@
+const express = require('express');
+const app = express();
+const mysql = require('mysql');
+const cors = require('cors');
+
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'classroom-management-system',
+    multipleStatements: true
+});
+
+app.listen(3001, ()=>{
+    console.log("backend server Running on port 3001");
+});
+
+app.post("/studentinfo", (req, res) => {
+    const registration = req.body.registration;
+    console.log(registration);
+  
+    db.query(
+      "SELECT * FROM student WHERE registration_no = ?",
+      [registration],
+      (err, result) => {
+        if (err) {
+          console.log(err);
+        } else {
+          res.send(result);
+        }
+      }
+    );
+  });
+
+app.post("/deptinfo", (req, res) => {
+  const departmentcode = req.body.departmentcode;
+  console.log("{ department code: "+departmentcode+" }");
+  
+  db.query(
+    "SELECT * FROM department WHERE department_code = ?",
+    [departmentcode],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+      } else {
+        res.send(result);
+      }
+    }
+  );
+});
+
+app.post("/createdept", (req, res) => {
+  const deptcode = req.body.deptcode;
+  const deptname = req.body.deptname;
+  const deptbuilding = req.body.deptbuilding;
+
+  db.query(
+    "INSERT INTO department (department_code, name, building) VALUES (?,?,?)",
+    [deptcode, deptname, deptbuilding],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+        res.send({err:1});
+      } else {
+        console.log("updated department database");
+        res.send({err:0});
+      }
+    }
+  );
+});
+
+app.post("/createstu", (req, res) => {
+  const reg = req.body.reg;
+  const name = req.body.name;
+  const session = req.body.session;
+  const department = req.body.department;
+
+  db.query(
+    "INSERT INTO student (registration_no, name, session, department) VALUES (?,?,?,?)",
+    [reg, name, session, department],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+        res.send({err:1});
+      } else {
+        console.log("inserted student database");
+        res.send({err:0});
+      }
+    }
+  );
+});
+
+app.put("/updatestu", (req, res) => {
+  const reg = req.body.reg;
+  const name = req.body.name;
+  const session = req.body.session;
+  const department = req.body.department;
+  db.query(
+    "UPDATE student SET name = ?,session = ?,department = ? WHERE registration_no = ?",
+    [name, session, department,reg],
+    (err, result) => {
+      if (err) {
+        console.log(err);
+        res.send({err:1});
+      } else {
+        console.log("updated Student");
+        res.send({err: 0});
+      }
+    }
+  );
+});
